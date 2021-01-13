afbd43711ab549ecd9cdec7022404143ff6638f6
adswqqe <xotjdbkdnj@naver.com>
2021-01-13 16:43:27 +0900

    Player FSMSystem 뼈대

---
+++ PS_RD/Assets/Tae/Script/FSM/CustomFSMSystem.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CustomFSMState
+{
+    Move,
+    Attack,
+    Jump,
+}
+
+public abstract class CustomFSMStateBase : IFSMStateBase
+{
+    private CustomFSMSystem _systemMgr = null;
+    public CustomFSMSystem SystemMgr => _systemMgr;
+
+    public CustomFSMStateBase(CustomFSMSystem system)
+    {
+        _systemMgr = system;
+    }
+
+    public abstract void StartState();
+
+    public abstract void Update();
+
+    public abstract void EndState();
+}
+
+public class CustomFSMSystem : FSMSystem<CustomFSMState, CustomFSMStateBase>
+{
+    private class MoveState : CustomFSMStateBase
+    {
+        public MoveState(CustomFSMSystem system) : base(system) { }
+
+        public override void EndState()
+        {
+        }
+
+        public override void StartState()
+        {
+        }
+
+        public override void Update()
+        {
+        }
+    }
+
+    private class AttackState : CustomFSMStateBase
+    {
+        public AttackState(CustomFSMSystem system) : base(system)
+        {
+        }
+
+        public override void EndState()
+        {
+        }
+
+        public override void StartState()
+        {
+        }
+
+        public override void Update()
+        {
+        }
+    }
+
+    private class JumpState : CustomFSMStateBase
+    {
+        public JumpState(CustomFSMSystem system) : base(system)
+        {
+        }
+
+        public override void EndState()
+        {
+        }
+
+        public override void StartState()
+        {
+        }
+
+        public override void Update()
+        {
+        }
+    }
+
+    protected override void RegisterState()
+    {
+
+    }
+}
---
+++ PS_RD/Assets/Tae/Script/FSM/FSMSystem.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IFSMStateBase
+{
+    void StartState();
+    void Update();
+    void EndState();
+
+}
+
+public abstract class FSMSystem<EnumType, State> : MonoBehaviour where EnumType : System.Enum where State : IFSMStateBase
+{
+    [SerializeField, ReadOnly]
+    private EnumType _currState;
+
+    public EnumType CurrState => _currState;
+
+    [SerializeField]
+    private EnumType _startState;
+    public EnumType StartState => _startState;
+
+    [SerializeField, ReadOnly]
+    private bool _isInit = false;
+    public bool IsInit => _isInit;
+
+    private Dictionary<EnumType, State> _fsmData = new Dictionary<EnumType, State>();
+
+    private void Awake()
+    {
+        InitState();
+    }
+
+    protected abstract void RegisterState();
+
+    private void InitState()
+    {
+        RegisterState();
+        ChangeState(StartState);
+        _isInit = true;
+    }
+
+    public void ChangeState(EnumType state)
+    {
+        if (_fsmData.ContainsKey(state) == false)
+            return;
+
+        if (IsInit == true)
+        {
+            if(_fsmData.ContainsKey(_currState) == true)
+            {
+                _fsmData[_currState].EndState();
+            }
+        }
+
+        _currState = state;
+        _fsmData[state].StartState();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsInit == false)
+            return;
+
+        if (_fsmData.ContainsKey(_currState) == true)
+            _fsmData[_currState].Update();
+    }
+
+    protected void AddState(EnumType type, State fsmState)
+    {
+        if (fsmState == null)
+            return;
+
+        if (_fsmData.ContainsKey(type) == true)
+        {
+            Debug.LogError("Have FSMState " + type.ToString());
+            return;
+        }
+
+        _fsmData.Add(type, fsmState);
+    }
+}
