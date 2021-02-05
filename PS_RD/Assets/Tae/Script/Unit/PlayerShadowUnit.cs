3fc4f2a22b2eee087c3f5222bb40e97d8db2bdde
adswqqe <xotjdbkdnj@naver.com>
2021-02-06 07:09:17 +0900

    공격 콜라이더 크리티컬 구현 및 그림자 출력 구현

---
+++ PS_RD/Assets/Tae/Script/Unit/PlayerShadowUnit.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShadowUnit : UnitBase
+{
+    private Unit _playerUnit;
+
+    private void Start()
+    {
+        _playerUnit = GetComponentInParent<Unit>();
+    }
+
+    public override void Progress()
+    {
+
+    }
+
+    public override void Attack()
+    {
+
+    }
+
+    public override void Idle()
+    {
+        Debug.Log(_playerUnit.CurAniState);
+        AniCtrl.PlayAni(_playerUnit.CurAniState);
+    }
+
+    public override void Dash()
+    {
+
+    }
+
+    public override void Jump(float height)
+    {
+
+    }
+
+    public override void JumpAttack()
+    {
+
+    }
+
+    public override void Move(float deltaX)
+    {
+
+    }
+
+    
+}
